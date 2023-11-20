a5478be5697707d759868e6865902739efed56ee
bayuweda <60813633+bayuweda@users.noreply.github.com>
2023-11-20 15:14:44 +0800

    Add files via upload

---
+++ tugasweb/2101020067_bayuweda.php
@@ -0,0 +1,12 @@
+<?php
+
+    $server     = "localhost";
+    $user       = "root";
+    $password   = "";
+    $database   = "2101020067_bayuweda";
+
+    $con = mysqli_connect($server,$user,$password,$database)
+
+?>
+
+
